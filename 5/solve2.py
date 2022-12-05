aa74cad2960e2c34b790ad60297504ad8577ee8b
justin <18606523+jlewis200@users.noreply.github.com>
2022-12-05 07:18:57 -0500

    Day 5.

---
+++ 5/solve2.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+import re
+
+with open("input.txt") as f_in:
+    line = f_in.readline()
+    n_stacks = len(line) // 4
+    stacks = [[] for _ in range((1 + n_stacks))]
+
+    while '[' in line:
+        jdx = 1
+
+        for idx in range(1, len(line), 4):
+            if line[idx] != ' ':
+                stacks[jdx].insert(0, line[idx])
+
+            jdx += 1
+
+        line = f_in.readline()
+
+    f_in.readline()
+
+    for line in f_in:
+        n, src, dst = list(map(int, re.findall(r"\d+", line)))
+        stacks[dst] += stacks[src][-n:]
+        stacks[src] = stacks[src][:-n]
+
+    for stack in stacks[1:]:
+        print(stack[-1], end='')
+
+    print()
+
+
