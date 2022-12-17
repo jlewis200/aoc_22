bc2265a67ceb75a1b0884187ea21a17c91f9e82e
justin <18606523+jlewis200@users.noreply.github.com>
2022-12-17 16:38:08 -0500

    Day 17.

--- 17/solve.py
+++ 17/solve.py
@@ -1,27 +1,25 @@
 #!/usr/bin/python3
 
+
 import numpy as np
 
-from code import interact
 
-rocks = [np.asarray([(0, 0), (1, 0), (2, 0), (3, 0)]),
+ROCKS = [np.asarray([(0, 0), (1, 0), (2, 0), (3, 0)]),
          np.asarray([(0, 1), (1, 1), (2, 1), (1, 2), (1, 0)]),
          np.asarray([(0, 0), (1, 0), (2, 0), (2, 1), (2, 2)]),
          np.asarray([(0, 0), (0, 1), (0, 2), (0, 3)]),
          np.asarray([(0, 0), (1, 0), (0, 1), (1, 1)])]
+ 
 
-
-def solve(filename, height):
+def solve(filename, n_rocks):
 
     with open(filename) as f_in:
         jets = list(f_in.readline().strip())
         jets = [(ord(char) - ord('='), 0) for char in jets]
 
-        #set of (x, y) tuples indicating occupied indices
+        #set of (x, y) tuples indicating occupied indices, initialize with floor
         occupied = set((idx, 0) for idx in range(1, 8))
 
-        #rock positions (x, y, type)
-        rock_positions = [(0, 0, -1)]
         maxs = [0]
 
         idx_jets = 0
@@ -29,13 +27,14 @@ def solve(filename, height):
         rock_count = 0
         y_max = 0
 
-        #floor at y=0
-
-        while rock_count < height:
-            rock = rocks[idx_rocks] + (3, y_max + 4) #initial offset
+        while rock_count < n_rocks:
+            rock = ROCKS[idx_rocks] + (3, y_max + 4) #initial offset
             rock_count += 1
+            rock_prime = rock
+            
+            while not rock_contact(rock_prime, occupied):
+                rock = rock_prime
 
-            while True:
                 #test wind movement
                 jet = jets[idx_jets]
                 idx_jets += 1
@@ -48,45 +47,94 @@ def solve(filename, height):
 
                 #test vertical movement
                 rock_prime = rock + (0, -1)
-                
-                if not rock_contact(rock_prime, occupied):
-                    rock = rock_prime
-
-                else:
-                    for coord in rock:
-                        occupied.add(tuple(coord))
-                    
-                    rock_positions.append(np.asarray(tuple(rock[0]) + (idx_rocks,)))
-
-                    y_max = max(y_max, rock[:, 1].max())
-                    break
+            
+            #add rock to occupied
+            for coord in rock:
+                occupied.add(tuple(coord))
+            
+            y_max = max(y_max, rock[:, 1].max())
 
             idx_rocks += 1
             idx_rocks %= 5
 
-            print(f"{rock_count:10} {y_max} ") 
             maxs.append(y_max)
         
-        print_rocks(occupied, y_max)
-        print(y_max) 
-        print()
+        #get the maxs/diffs per rock iteration
+        maxs = np.asarray(maxs)
+        diffs = get_diffs(maxs)      
         
-        for idx in range(len(rock_positions) - 1, 0, -1):
-            #print(rock_positions[idx])
-            rock_positions[idx][:2] -= rock_positions[idx - 1][:2]
+        #get the indices of a diff repeating sequence
+        idx_0, idx_1 = repeated_substring(diffs)
+        seq = diffs[idx_0 : idx_1 + 1]
+        
+        n_rocks = 1000000000000
+       
+        #number of full sequences required to reach n_rocks
+        n_full_sequences = (n_rocks - idx_0) // seq.shape[0]
+        
+        #number of elements of the partial sequence required to reach n_rocks
+        len_partial_sequence = (n_rocks - idx_0) % seq.shape[0]
+
+        #the target value is composed of the max height of the rock before the first sequence
+        #plus the number of full sequences times the sum of diffs in the sequence
+        #plus the sum of the length of the partial sequence which doesn't evenly divide n_rocks
+        y_max = maxs[idx_0 - 1] + \
+                (n_full_sequences * sum(seq)) + \
+                sum(seq[:len_partial_sequence + 1])
+
+        print(maxs[2022])
+        print(y_max)
+        print()
 
 
-        print('#'*80)
+def get_diffs(maxs):
+    """
+    Get the max height difference from one iteration to the next.
+    """
 
-        for idx in range(len(rock_positions) - 1, 0, -1):
-            rock = rock_positions[idx]
+    diffs = np.zeros_like(maxs)
+    
+    for idx in range(1, len(maxs)):
+        diffs[idx] = maxs[idx] - maxs[idx - 1]
 
-            print(f"{idx:3}     {rock[0]:3} {rock[1]:3} {rock[2]:3}")
+    return diffs
+
+
+def repeated_substring(diffs):
+    """
+    Get the start/end index of a cycle in the diffs.
+    """
+
+    #start with a short sequence on the end of the diffs
+    for jdx in range(2, diffs.shape[0]):
+        seq_indices = []
+
+        #take the last jdx values from diff as the candidate sequence
+        seq = diffs[-jdx:]
+
+        #find the indices in diffs where the subsequence matches
+        idx_0 = 0
+
+        while idx_0 <= diffs.shape[0] - jdx:
+            idx_1 = idx_0 + jdx
             
-        for idx in range(len(jets), len(maxs) + 1, len(jets)):
-            diff = maxs[idx] - maxs[idx - len(jets)]
-            print(f"{idx:10} {diff} ") 
-        interact(local=locals())
+            if np.array_equal(diffs[idx_0 : idx_1], seq):
+                #add subsequences to the list
+                seq_indices.append((idx_0, idx_1))
+                idx_0 = idx_1 + 1
+
+            else:
+                idx_0 += 1
+
+        print(seq_indices)
+   
+        #if all of the sequences are adjacent, these are the maximal subsequence without repetition
+        if all([seq_indices[idx - 1][1] == seq_indices[idx][0] - 1 for idx in range(1, len(seq_indices))]):
+            break
+
+    #return the first sequence (any of them will work)
+    return seq_indices[0]
+
 
 def print_rocks(occupied, y_max):
     for idx in range(y_max + 1, 0, -1):
@@ -100,10 +148,10 @@ def print_rocks(occupied, y_max):
                 row += '.'
 
         row += '|'
+   
         print(row)
+    
     print("+-------+")
-
-
 
 
 def rock_contact(rock_prime, occupied):
@@ -118,9 +166,5 @@ def rock_contact(rock_prime, occupied):
 
 
 if __name__ == "__main__":
-    #solve("test_input.txt", 2022)
-    #solve("input.txt", 2022)
-    solve("test_input.txt", 1000)
-    
-    #solve("test_input.txt", 1000000000000)
-    #solve("input.txt", 2022)
+    solve("test_input.txt", 2022)
+    solve("input.txt", 5000)
